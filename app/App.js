427ce6bfd7714ad9656bbcb88ad6aa3faa96a4d5
Miguel-Eduardo-Chacon <miguel.chacon@samishop.pe>
2023-10-20 11:53:32 -0500

    remove app.js

--- app/App.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "./js/Router.js";
-import { Template } from "./layouts/Template.js";
-
-export const App = async () => {
-  const $root = document.getElementById('root');
-  $root.innerHTML = ''
-
-  $root.innerHTML = await Template();
-}
